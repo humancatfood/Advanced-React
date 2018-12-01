37b0ef3a8a43e5a7da732c8bbc3c6d56bf5cdfac
Jan Borchers <humancatfood@users.noreply.github.com>
2018-12-01 15:46:53 +0000

    Setup and add styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -3,6 +3,8 @@ import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
+
+
 const theme = {
   red: '#FF0000',
   black: '#393939',
