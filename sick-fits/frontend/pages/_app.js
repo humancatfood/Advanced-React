25f07494c3bf0019e7db3fb8957c621b94412333
Jan Borchers <humancatfood@users.noreply.github.com>
2018-12-16 23:53:08 +0000

    Hook up Apollo Client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,22 +1,39 @@
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+
+import withData from '../lib/withData';
+
 import Page from '../components/Page';
+
 
 
 
 class MyApp extends App {
 
+  static async getInitialProps ({ Component, ctx }) {
+    return {
+      pageProps: {
+        ... (Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+        query: ctx.query,
+      }
+    };
+  }
+
   render() {
-    const { Component } = this.props;
+
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 
 }
 
-export default MyApp;
+export default withData(MyApp);
