e9a82ce7d3825415d183e208fd6412dadd591614
Jan Borchers <humancatfood@users.noreply.github.com>
2019-01-18 20:05:14 +0000

    Add Sign *IN* Flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
 // const { v4: uuid } = require('uuid');
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 const { sign } = require('jsonwebtoken');
 const { forwardTo } = require('prisma-binding');
 
@@ -9,6 +9,7 @@ module.exports = {
   createItem: forwardTo('db'),
   updateItem: forwardTo('db'),
   deleteItem: forwardTo('db'),
+
   signup: async (parent, args, ctx, info) => {
 
     const email = args.email.toLowerCase().trim();
@@ -35,7 +36,32 @@ module.exports = {
 
     return user;
 
-  }
+  },
+
+  signin: async (parent, args, ctx/* , info */) => {
+
+    const email = args.email.toLowerCase().trim();
+
+    const user = await ctx.db.query.user({
+      where: {
+        email,
+      }
+    });
+
+    if (user && await compare(args.password, user.password)) {
+      const token = sign({ userID: user.id }, process.env.APP_SECRET);
+      ctx.response.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+      });
+      return user;
+    } else {
+      throw new Error('Incorrect email/password combination');
+    }
+
+  },
+
+
   // createItem: async (parent, args, ctx, info) => {
   //   const newItem = await ctx.db.mutation.createItem({
   //     data: {
--- sick-fits/frontend/pages/signup.js
+++ sick-fits/frontend/pages/signup.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Signup from '../components/Signup';
+import Signin from '../components/Signin';
 
 
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
 
 const SignupPage = () => (
-  <Signup />
+  <Columns>
+    <Signup />
+    <Signin />
+    {/* <RequestReset /> */}
+  </Columns>
 );
 
 export default SignupPage;
